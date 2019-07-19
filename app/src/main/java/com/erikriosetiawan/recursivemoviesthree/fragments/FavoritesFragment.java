9ccaf7e9e4392cd984d911c5eaa8425aa2043f53
erik1997 <erikriosetiawan49@gmail.com>
2019-07-19 20:59:36 +0700

    Adding favorite fragment

--- app/src/main/java/com/erikriosetiawan/recursivemoviesthree/fragments/FavoritesFragment.java
+++ app/src/main/java/com/erikriosetiawan/recursivemoviesthree/fragments/FavoritesFragment.java
@@ -2,18 +2,30 @@ package com.erikriosetiawan.recursivemoviesthree.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.erikriosetiawan.recursivemoviesthree.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavoritesFragment extends Fragment {
 
+    private ViewPager viewPager;
 
     public FavoritesFragment() {
         // Required empty public constructor
@@ -24,7 +36,43 @@ public class FavoritesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_favorites, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewPager = getActivity().findViewById(R.id.pager);
+        viewPager.setAdapter(new ViewPagerAdapter(getFragmentManager()));
+
+        PagerSlidingTabStrip tabs = getActivity().findViewById(R.id.tabs);
+        tabs.setShouldExpand(true);
+        tabs.setIndicatorColor(getResources().getColor(R.color.colorPrimaryDark));
+        tabs.setViewPager(viewPager);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return (position == 0) ? getResources().getString(R.string.nav_movies) : getResources().getString(R.string.nav_tv_shows);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return (i  == 0)? new MoviesFragment() : new TvShowsFragment();
+        }
+
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
 }
