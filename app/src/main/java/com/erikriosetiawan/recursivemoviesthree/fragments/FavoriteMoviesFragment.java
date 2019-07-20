90fbef0f8714cf9991ef60580134622b4bc851fb
erik1997 <erikriosetiawan49@gmail.com>
2019-07-20 17:24:26 +0700

    Adding Database

--- app/src/main/java/com/erikriosetiawan/recursivemoviesthree/fragments/FavoriteMoviesFragment.java
+++ app/src/main/java/com/erikriosetiawan/recursivemoviesthree/fragments/FavoriteMoviesFragment.java
@@ -1,19 +1,32 @@
 package com.erikriosetiawan.recursivemoviesthree.fragments;
 
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.erikriosetiawan.recursivemoviesthree.R;
+import com.erikriosetiawan.recursivemoviesthree.adapters.FavoriteMovieAdapter;
+import com.erikriosetiawan.recursivemoviesthree.db.FavoriteMoviesDatabaseHelper;
+import com.erikriosetiawan.recursivemoviesthree.models.Movie;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FavoriteMoviesFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private FavoriteMovieAdapter favoriteMovieAdapter;
+    private ArrayList<Movie> movies = new ArrayList<>();
+    private FavoriteMoviesDatabaseHelper favoriteMoviesDatabaseHelper;
 
     public FavoriteMoviesFragment() {
         // Required empty public constructor
@@ -24,7 +37,41 @@ public class FavoriteMoviesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_movies, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_favorite_movies, container, false);
+        favoriteMoviesDatabaseHelper = new FavoriteMoviesDatabaseHelper(getActivity());
+        recyclerView = rootView.findViewById(R.id.recycler_view_fragment_favorite_movie);
+        getAllFavorite();
+        setRecyclerList();
+        return rootView;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void getAllFavorite() {
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                movies.clear();
+                movies.addAll(favoriteMoviesDatabaseHelper.getAllFavorite());
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                favoriteMovieAdapter.notifyDataSetChanged();
+            }
+        }.execute();
+    }
+
+    private void setRecyclerList() {
+        if (favoriteMovieAdapter == null) {
+            favoriteMovieAdapter = new FavoriteMovieAdapter(getActivity(), movies);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerView.setAdapter(favoriteMovieAdapter);
+        } else {
+            favoriteMovieAdapter.notifyDataSetChanged();
+        }
     }
 
 }
