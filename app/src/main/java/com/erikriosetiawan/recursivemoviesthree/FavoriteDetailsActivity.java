fad52633ad3a92ad0a5aed0d537aa2d564b4d7c5
erik1997 <erikriosetiawan49@gmail.com>
2019-07-22 15:32:21 +0700

    Add action when favorite list clicked

--- app/src/main/java/com/erikriosetiawan/recursivemoviesthree/FavoriteDetailsActivity.java
+++ app/src/main/java/com/erikriosetiawan/recursivemoviesthree/FavoriteDetailsActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.squareup.picasso.Picasso;
 
 public class FavoriteDetailsActivity extends AppCompatActivity {
